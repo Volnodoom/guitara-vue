4281645665dd9c5c6ef65314545093f8e64aee8e
Alexander <bdiyoz@gmail.com>
2024-09-07 16:39:10 +0300

    [REFACTOR] head search block

--- ui/custom-header/components/search-form.vue
+++ ui/custom-header/components/search-form.vue
@@ -3,19 +3,7 @@
     id="form-search"
     class="form-search__form"
   >
-    <button
-      class="form-search__submit"
-      type="submit"
-    >
-      <svg
-        class="form-search__icon"
-        width="14"
-        height="15"
-        aria-hidden="true"
-      >
-        <use xlink:href="#icon-search" />
-      </svg><span class="visually-hidden">Начать поиск</span>
-    </button>
+    <SearchSubmitBtn />
     <input
       id="search"
       class="form-search__input"
@@ -30,8 +18,6 @@
   </form>
 </template>
 
-<script>
-export default {
-
-}
+<script setup lang="ts">
+import SearchSubmitBtn from '~/ui/custom-header/components/search-submit-btn.vue';
 </script>
---
+++ ui/custom-header/components/search-submit-btn.vue
@@ -0,0 +1,19 @@
+<template>
+  <button
+    class="form-search__submit"
+    type="submit"
+  >
+    <svg
+      class="form-search__icon"
+      width="14"
+      height="15"
+      aria-hidden="true"
+    >
+      <use xlink:href="#icon-search" />
+    </svg><span class="visually-hidden">Начать поиск</span>
+  </button>
+</template>
+
+<script setup lang="ts">
+
+</script>
